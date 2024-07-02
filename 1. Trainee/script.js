8ce3530e8bf8c22c01967325a96fb70ecf8cbae9
PaulaRamosLeon <ramosleonpaula@alumnos.cei.es>
2024-07-02 22:55:45 +0200

    actividad antes de optimizaciones

---
+++ 1. Trainee/script.js
@@ -0,0 +1,110 @@
+// Declarar constantes
+
+    //Imágenes
+const zpump = document.getElementById("zpump");
+const fontvella = document.getElementById("fontvella");
+const rcar = document.getElementById("rcar");
+const pulse = document.getElementById("pulse");
+const alhambra = document.getElementById("alhambra");
+const actimel = document.getElementById("actimel");
+const ots = document.getElementById("ots");
+const delicious = document.getElementById("delicious");
+const roca = document.getElementById("roca");
+const state = document.getElementById("state");
+
+    //Divs
+const zpump_div = document.getElementById("zpump_div");
+const fontvella_div = document.getElementById("fontvella_div");
+const rcar_div = document.getElementById("rcar_div");
+const pulse_div = document.getElementById("pulse_div");
+const alhambra_div = document.getElementById("alhambra_div");
+const actimel_div = document.getElementById("actimel_div");
+const ots_div = document.getElementById("ots_div");
+const delicious_div = document.getElementById("delicious_div");
+const roca_div = document.getElementById("roca_div");
+const state_div = document.getElementById("state_div");
+
+
+// añadir EventListener a los divs
+zpump_div.addEventListener('mouseover', function() {
+    zpump.classList.add('visible');
+});
+
+zpump_div.addEventListener('mouseout', function() {
+    zpump.classList.remove('visible');
+});
+
+// ------------------------------ //
+fontvella_div.addEventListener('mouseover', function() {
+    fontvella.classList.add('visible');
+});
+
+fontvella_div.addEventListener('mouseout', function() {
+    fontvella.classList.remove('visible');
+});
+// ------------------------------ //
+rcar_div.addEventListener('mouseover', function() {
+    rcar.classList.add('visible');
+});
+
+rcar_div.addEventListener('mouseout', function() {
+    rcar.classList.remove('visible');
+});
+// ------------------------------ //
+pulse_div.addEventListener('mouseover', function() {
+    pulse.classList.add('visible');
+});
+
+pulse_div.addEventListener('mouseout', function() {
+    pulse.classList.remove('visible');
+});
+// ------------------------------ //
+alhambra_div.addEventListener('mouseover', function() {
+    alhambra.classList.add('visible');
+});
+
+alhambra_div.addEventListener('mouseout', function() {
+    alhambra.classList.remove('visible');
+});
+
+// ------------------------------ //
+actimel_div.addEventListener('mouseover', function() {
+    actimel.classList.add('visible');
+});
+
+actimel_div.addEventListener('mouseout', function() {
+    actimel.classList.remove('visible');
+});
+// ------------------------------ //
+ots_div.addEventListener('mouseover', function() {
+    ots.classList.add('visible');
+});
+
+ots_div.addEventListener('mouseout', function() {
+    ots.classList.remove('visible');
+});
+// ------------------------------ //
+delicious_div.addEventListener('mouseover', function() {
+    delicious.classList.add('visible');
+});
+
+delicious_div.addEventListener('mouseout', function() {
+    delicious.classList.remove('visible');
+});
+
+// ------------------------------ //
+roca_div.addEventListener('mouseover', function() {
+    roca.classList.add('visible');
+});
+
+roca_div.addEventListener('mouseout', function() {
+    roca.classList.remove('visible');
+});
+// ------------------------------ //
+state_div.addEventListener('mouseover', function() {
+    state.classList.add('visible');
+});
+
+state_div.addEventListener('mouseout', function() {
+    state.classList.remove('visible');
+});
